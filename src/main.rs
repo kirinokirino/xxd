9d4357d10c0eee88d8ac64c78f8dc0afc27b4a8c
kirinokirino <kirinokirino@lunariko.com>
2023-04-07 20:50:44 +0300

    Impl hex and reverese modes.

--- src/main.rs
+++ src/main.rs
@@ -52,12 +52,12 @@ fn read_contents(reader: &mut io::BufReader<File>, mode: Mode) {
     for (i, bytes) in buffer.array_chunks::<16>().enumerate() {
         match mode {
             Mode::Graphical => {
-                let hex = format_hex(bytes);
+                let hex = format_hex(bytes, true);
                 let text = format_text(bytes);
                 println!("{i:0>8x}: {hex} {text}");
             }
-            Mode::Hex => todo!(),
-            Mode::Reverse => todo!(),
+            Mode::Hex => print!("{}", format_hex(bytes, false)),
+            Mode::Reverse => print!("{}", parse_bytes(bytes)),
         }
     }
     let bytes_read = buffer.len();
@@ -77,9 +77,23 @@ fn format_text(bytes: &[u8; 16]) -> String {
         .collect()
 }
 
-fn format_hex(bytes: &[u8; 16]) -> String {
+fn format_hex(bytes: &[u8; 16], add_space: bool) -> String {
+    let space = if add_space { " " } else { "" };
     bytes
-        .map(|byte| format!("{byte:0>2x} "))
+        .map(|byte| format!("{byte:0>2x}{space}"))
         .into_iter()
+        .collect()
+}
+
+fn parse_bytes(bytes: &[u8; 16]) -> String {
+    bytes
+        .array_chunks::<2>()
+        .map(|[first, second]| {
+            let first = u8::from_str_radix(&(*first as char).to_string(), 16)
+                .expect("In reverse mode only [0-9a-f] chars are allowed");
+            let second = u8::from_str_radix(&(*second as char).to_string(), 16)
+                .expect("In reverse mode only [0-9a-f] chars are allowed");
+            (first << 4 | second) as char
+        })
         .collect()
 }
